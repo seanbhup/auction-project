47c8c3e1d521de0713c806a551ed9c658c49e514
Sean Bhupathi <seanbhupathi@gmail.com>
2017-02-15 10:59:33 -0500

    mysql base setup

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+var config = require("../config/config.js");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+var connection = mysql.createConnection ({
+    host: config.host,
+    user: config.user,
+    password: config.password,
+    database: config.database
+});
+
+connection.connect();
+
+
+
+/* GET top auctions. */
+router.get('/getHomeAuctions', function(req, res, next) {
+    var auctionsQuery = "select * from auctions inner join images on images.auction_id = auctions.id limit 10";
+    connection.query(auctionsQuery, (error,results,fields)=>{
+        if (error) throw error;
+        res.json(results);
+    })
 });
 
 module.exports = router;
