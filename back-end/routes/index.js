b293b12b3c714db9a529bf04bca163687ad18031
Sean Bhupathi <seanbhupathi@gmail.com>
2017-02-16 10:16:02 -0500

    bcrypt added

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -12,8 +12,17 @@ var connection = mysql.createConnection ({
 
 connection.connect();
 
+var bcrypt = require('bcrypt-nodejs');
+
+// var hashedPassword = bcrypt.hashSync("x");
+// console.log(hashedPassword);
+// var checkHash = bcrypt.compareSync("x", hashedPassword);
+// console.log(checkHash);
+
 router.get('/getHomeAuctions', (req, res, next)=>{
+
     var auctionsQuery = "select * from auctions inner join images on images.auction_id = auctions.id limit 10";
+
     connection.query(auctionsQuery, (error,results,fields)=>{
         if (error) throw error;
         res.json(results);
@@ -21,12 +30,17 @@ router.get('/getHomeAuctions', (req, res, next)=>{
 });
 
 router.post('/register', (req,res,next)=>{
+    var email = req.body.email;
+    var username = req.body.username;
+    var password = bcrypt.hashSync(req.body.password);
+
     var checkDupeUserQuery = "select * from users where username = ?";
-    connection.query(checkDupeUserQuery, [req.body.username], (error,results,fields)=>{
+
+    connection.query(checkDupeUserQuery, [username], (error,results,fields)=>{
         if (error) throw error;
         if(results.length === 0){
             var insertUserQuery = "insert into users (email, username, password) values (?,?,?)";
-            connection.query(insertUserQuery, [req.body.email,req.body.username,req.body.password], (error2,results2,fields2)=>{
+            connection.query(insertUserQuery, [email,username,password], (error2,results2,fields2)=>{
                 if (error2) throw error2;
                 res.json({msg:"userInserted"});
             });
